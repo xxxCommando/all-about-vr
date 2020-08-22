8d87e8b6cfe3c67333b9a159eb1de3c3f94d3e35
loic <loic.600@hotmail.fr>
2020-08-22 23:10:31 +0200

    Ajout data firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
 
 function App() {
+
+
+//get elements
+const oculusRiftObject = document.getElementById('oculusRift');
+const htcViveObject = document.getElementById('htcVive');
+
+
+//create references
+const dbRefOculusRift = firebase.database().ref().child('oculusrift');
+const dbRefHtcVive = firebase.database().ref().child('htcvive');
+
+
+//sync object change
+dbRefOculusRift.on('value', snap => {oculusRiftObject.innerText = JSON.stringify(snap.val(), null, 3)});
+dbRefHtcVive.on('value', snap => {htcViveObject.innerText = JSON.stringify(snap.val(), null, 3)});
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+
+
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+
+//using dotenv to manage env variable
+//require('dotenv').config();
+
+require('dotenv').config()
+
+
+let apiKey = process.env.REACT_APP_APIKEY;
+let authDomain = process.env.REACT_APP_AUTHDOMAIN;
+let databaseURL = process.env.REACT_APP_DATABASEURL;
+let projectId = process.env.REACT_APP_PROJECTID;
+let storageBucket = process.env.REACT_APP_STORAGEBUCKET;
+let messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID;
+let appId = process.env.REACT_APP_APPID;
+let measurementId = process.env.REACT_APP_MEASUREMENTID;
+
+
+const config = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
+};
+
+
+
+firebase.initializeApp(config);
+
 
 ReactDOM.render(
   <React.StrictMode>
