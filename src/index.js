9974724bd8e4a5296426f693d19b3a84d1e7b0be
xxCommando <68127762+xxCommando@users.noreply.github.com>
2020-08-28 16:49:37 +0200

    create and add data (#8)
    * Ajout data firebase
    
    * initial commit
    
    * indentation avec prettier + .en.example ajouté
    
    * Use airbnb eslint
    
    * Fix docker production environment
    
    * fix .env.example and eslint
    
    Co-authored-by: DOUIB Bastien <bastiendouib@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as firebase from 'firebase';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// using dotenv to manage env variable
+
+require('dotenv').config();
+
+const apiKey = process.env.REACT_APP_APIKEY;
+const authDomain = process.env.REACT_APP_AUTHDOMAIN;
+const databaseURL = process.env.REACT_APP_DATABASEURL;
+const projectId = process.env.REACT_APP_PROJECTID;
+const storageBucket = process.env.REACT_APP_STORAGEBUCKET;
+const messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID;
+const appId = process.env.REACT_APP_APPID;
+const measurementId = process.env.REACT_APP_MEASUREMENTID;
+
+const config = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
