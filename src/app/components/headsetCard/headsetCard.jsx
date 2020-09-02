04d63c11d95e6d946cc4265881774e7f281a1941
DOUIB Bastien <bastiendouib@gmail.com>
2020-09-02 18:39:05 +0200

    Refactor card headset link (css)

--- src/app/components/headsetCard/headsetCard.jsx
+++ src/app/components/headsetCard/headsetCard.jsx
@@ -12,26 +12,27 @@ const HeadsetCard = (props) => {
     selectForCompare, item, compareMode, onClick, children,
   } = props;
   return (
-    <Card
-      onClick={
-        compareMode && !selectForCompare ? () => {} : () => onClick(item.id)
-      }
-      hoverable
-      bordered
-      className={selectForCompare ? 'selected' : ''}
-      title={item.name}
-      cover={<Image src={item.img} preview={false} />}
-      extra={(
-        <FileSearchOutlined
-          key="information"
-          onClick={() => console.log('router vers la page')}
-        />
-      )}
-    >
-      {
-        children || <Card.Meta description={item.summary} title={`${item.price} $`} />
-      }
-    </Card>
+    <div>
+      <Card
+        onClick={
+          compareMode && !selectForCompare ? () => {} : () => onClick(item.id)
+        }
+        hoverable
+        bordered
+        className={selectForCompare ? 'selected' : ''}
+        title={item.name}
+        cover={<Image src={item.img} preview={false} />}
+      >
+        {
+          children || <Card.Meta description={item.summary} title={`${item.price} $`} />
+        }
+      </Card>
+      <FileSearchOutlined
+        key="information"
+        onClick={() => console.log('router vers la page')}
+        className="card-information-link"
+      />
+    </div>
   );
 };
 
