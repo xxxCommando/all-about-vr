2805fcdd6da02db78cd5f4cffb0eb0129ac97ca5
DOUIB Bastien <bastiendouib@gmail.com>
2020-10-03 19:46:52 +0200

    Add link in footer

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -3,6 +3,13 @@ import { Layout } from 'antd';
 
 import './footer.scss';
 
-const Footer = () => <Layout.Footer>AllAboutVR©2020 Created by Bleuh and xxCommando</Layout.Footer>;
+const Footer = () => (
+  <Layout.Footer>
+    <span>AllAboutVR©2020 Created by </span>
+    <a href="https://github.com/Bleuh" target="_blank" rel="noreferrer">Bleuh</a>
+    <span> and </span>
+    <a href="https://github.com/xxCommando" target="_blank" rel="noreferrer">xxCommando</a>
+  </Layout.Footer>
+);
 
 export default Footer;
