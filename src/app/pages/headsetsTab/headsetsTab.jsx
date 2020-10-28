331fe9f76fda661007d88643f3d6bfbf543297c9
loic <loic.600@hotmail.fr>
2020-10-28 16:29:53 +0100

    Drawer et Sider Ok, tableau pas ok en mobile

--- src/app/pages/headsetsTab/headsetsTab.jsx
+++ src/app/pages/headsetsTab/headsetsTab.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Typography, Layout, Divider, Table, Menu, Image,
+  Typography, Layout, Divider, Table, Image,
 } from 'antd';
 import { Resizable } from 'react-resizable';
 import './headsetsTab.scss';
 import { HeadsetShape } from '../../shape';
+import FilterDrawer from '../../components/filterDrawer';
+import FilterSider from '../../components/filterSider';
 
 const { Sider } = Layout;
-
-const { SubMenu } = Menu;
 
 const ResizableTitle = (props) => {
   const {
@@ -134,39 +134,16 @@ class HeadsetsTab extends React.Component {
 
         return (
           <>
-            <Layout>
+            <Layout className="headsetTab">
 
-              <Sider width={200} className="site-layout-background">
-                <Menu
-                  mode="inline"
-                  defaultSelectedKeys={['1']}
-                  defaultOpenKeys={['sub1']}
-                  style={{ height: '100%', borderRight: 0 }}
-                >
-                  <SubMenu key="sub1" title="subnav 1">
-                    <Menu.Item key="1">option1</Menu.Item>
-                    <Menu.Item key="2">option2</Menu.Item>
-                    <Menu.Item key="3">option3</Menu.Item>
-                    <Menu.Item key="4">option4</Menu.Item>
-                  </SubMenu>
-                  <SubMenu key="sub2" title="subnav 2">
-                    <Menu.Item key="5">option5</Menu.Item>
-                    <Menu.Item key="6">option6</Menu.Item>
-                    <Menu.Item key="7">option7</Menu.Item>
-                    <Menu.Item key="8">option8</Menu.Item>
-                  </SubMenu>
-                  <SubMenu key="sub3" title="subnav 3">
-                    <Menu.Item key="9">option9</Menu.Item>
-                    <Menu.Item key="10">option10</Menu.Item>
-                    <Menu.Item key="11">option11</Menu.Item>
-                    <Menu.Item key="12">option12</Menu.Item>
-                  </SubMenu>
-                </Menu>
+              <Sider width={250} className="sider">
+                <FilterSider />
               </Sider>
               <Layout.Content className="layout-content">
 
-                <Typography.Title level={1}>Headsets</Typography.Title>
+                <Typography.Title level={1} className="title">Headsets</Typography.Title>
                 <Divider />
+                <div className="filter"><FilterDrawer /></div>
 
                 <Table bordered components={this.components} columns={columns} dataSource={data(items)} />
               </Layout.Content>
