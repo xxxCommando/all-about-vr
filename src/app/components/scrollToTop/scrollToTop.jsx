f8a51fd67e428a34c54e26fcb6f161c983ac1aac
DOUIB Bastien <bastiendouib@gmail.com>
2020-11-04 15:33:54 +0100

    Re-add scroll top if no hash

--- src/app/components/scrollToTop/scrollToTop.jsx
+++ src/app/components/scrollToTop/scrollToTop.jsx
@@ -1,12 +1,26 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-export default function ScrollToTop() {
-  const { pathname } = useLocation();
+class ScrollToTop extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { location } = this.props;
+    if (location.pathname !== prevProps.location.pathname && location.hash === '') {
+      console.log('scroll');
+      window.scrollTo(0, 0);
+    }
+  }
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
+  render() {
+    return null;
+  }
 }
+
+ScrollToTop.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+    hash: PropTypes.string,
+  }).isRequired,
+};
+
+export default withRouter(ScrollToTop);
